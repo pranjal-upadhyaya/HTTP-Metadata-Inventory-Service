aab39462b90675a2491c2d7f0ac4dd3e44a03af3
pranjal <rktpranjal@gmail.com>
2026-04-13 00:29:51 +0530

    removed redundant code from connection_manager

--- db/connection_manager.py
+++ db/connection_manager.py
@@ -1,14 +1,19 @@
-from pymongo import MongoClient
+from pymongo import AsyncMongoClient
+
 from app.config import app_config
 
 
 class ConnectionManager:
-
-    def __init__(self) -> None:
-        self.client = MongoClient(
-            host=app_config.db_host,
-            port=app_config.db_port,
+    def get_async_client(self) -> AsyncMongoClient:
+        c = app_config
+        return AsyncMongoClient(
+            host=c.db_host,
+            port=c.db_port,
+            maxPoolSize=c.db_max_pool_size,
+            minPoolSize=c.db_min_pool_size,
+            maxIdleTimeMS=c.db_max_idle_time_ms,
+            connectTimeoutMS=c.db_connect_timeout_ms,
+            socketTimeoutMS=c.db_socket_timeout_ms,
+            serverSelectionTimeoutMS=c.db_server_selection_timeout_ms,
+            waitQueueTimeoutMS=c.db_wait_queue_timeout_ms,
         )
-
-    def get_client(self):
-        return self.client
